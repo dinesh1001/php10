2f3e20b736efb11d4ea95b580ef081f6df5b77de
root <root@openshift.example.com>
2017-02-10 13:56:06 +0530

    MY GIT CHNAGES

---
+++ radio.php
@@ -0,0 +1,8 @@
+<html>
+<head><title></title></head>
+<body>
+<?php
+echo "You selected the answer: $_POST[Question1]";
+?>
+</body>
+</html>
---
+++ text.php
@@ -0,0 +1,9 @@
+<html>
+<head><title></title></head>
+<body>
+Your favorite web site is:
+<?php
+echo $_POST['WebSites'];
+?>
+</body>
+</html>
