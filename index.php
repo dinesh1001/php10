8e13d1f1fe39be2ef16e129de8332f9077247fa9
bhalendra <rathore.bhalendra@gmail.com>
2017-06-13 12:22:20 +0530

    Create index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-ITE - Institute for Technical Education
-Authorized Red Hat Training Partner
+NITTTR
+Authoriized Red Hat Training Partner
 131/13 second floor Zone-II
 M.P. Nagar Bhopal 462011
 con. 0755-4700646, 9111240646
